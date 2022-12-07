1e9bfe1b623e72920f007041aa43db36f95695f9
Raulo <rauleduardovigil@gmail.com>
2022-12-07 15:24:01 -0600

    installing codeigniter4 for api

---
+++ bd.sql
@@ -0,0 +1,27 @@
+create database prueba_raul;
+use prueba_raul;
+
+CREATE TABLE productos(
+producto_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+producto_nombre VARCHAR(100)UNIQUE NOT NULL,
+producto_descripcion VARCHAR(200) NOT NULL,
+producto_imagen_url TEXT NOT NULL,
+producto_precio VARCHAR(10) NOT NULL,
+PRIMARY KEY(producto_id)
+);
+
+CREATE TABLE pedidos(
+pedido_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+pedido_nombre_cliente VARCHAR(50) NOT NULL,
+pedido_fecha_pedido DATE NOT NULL,
+PRIMARY KEY(pedido_id)
+);
+
+CREATE TABLE pedidos_detalle(
+pedidos_detalle_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+pedidos_detalle_pedido_id INT UNSIGNED NOT NULL,
+pedidos_detalle_producto_id INT UNSIGNED NOT NULL,
+CONSTRAINT fk_pedidosdetalles_pedidos FOREIGN KEY (pedidos_detalle_pedido_id) REFERENCES pedidos(pedido_id),
+CONSTRAINT fk_pedidosdetalles_productos FOREIGN KEY(pedidos_detalle_producto_id) REFERENCES productos(producto_id),
+PRIMARY KEY(pedidos_detalle_id)
+);
